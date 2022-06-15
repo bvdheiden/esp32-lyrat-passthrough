4335a424d664f4fae668747a18dc1cf8b7f24ef2
Ben van der Heiden <b.vdh@protonmail.com>
2022-06-15 22:57:30 +0200

    tweak values

--- main/dsp_filter.c
+++ main/dsp_filter.c
@@ -3,16 +3,18 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
-#define BANDWIDTH_VAL 0.75f
+#define BANDWIDTH_GAIN 0.995f
+#define DAMPING_GAIN 1.0f - BANDWIDTH_GAIN
 
 #define INPUT_DIFFUSION_1_2_GAIN 0.750f
 #define INPUT_DIFFUSION_3_4_GAIN 0.625f
 
-#define DECAY_DIFFUSION_1_GAIN 0.7f
-#define DECAY_DIFFUSION_2_GAIN 0.5f
-
-#define DAMPING_GAIN 0.0005f
+// #define DECAY_DIFFUSION_1_GAIN 0.7f
+#define DECAY_DIFFUSION_1_GAIN 0.5f
+// #define DECAY_DIFFUSION_2_GAIN 0.5f
+#define DECAY_DIFFUSION_2_GAIN 0.4f
 
 // use smaller buffer lengths
 #define INPUT_DIFFUSION_1_BUFFER_LENGTH 142
@@ -27,6 +29,8 @@
 #define DAMPING_DELAY_2_BUFFER_LENGTH 3720
 
 int16_t BANDWIDTH_FILTER_BUFFER = 0;
+int16_t DAMPING_FILTER_BUFFER = 0;
+int16_t REVERBERANCE_BUFFER = 0;
 
 int16_t INPUT_DIFFUSION_1_BUFFER[INPUT_DIFFUSION_1_BUFFER_LENGTH];
 dsp_ring_buffer_t INPUT_DIFFUSION_1_RINGBUFFER;
@@ -37,21 +41,16 @@ dsp_ring_buffer_t INPUT_DIFFUSION_3_RINGBUFFER;
 int16_t INPUT_DIFFUSION_4_BUFFER[INPUT_DIFFUSION_4_BUFFER_LENGTH];
 dsp_ring_buffer_t INPUT_DIFFUSION_4_RINGBUFFER;
 
-dsp_ring_buffer_t DECAY_DIFFUSION_1_RINGBUFFER;
 int16_t DECAY_DIFFUSION_1_BUFFER[DECAY_DIFFUSION_1_BUFFER_LENGTH];
-dsp_ring_buffer_t DECAY_DIFFUSION_2_RINGBUFFER;
+dsp_ring_buffer_t DECAY_DIFFUSION_1_RINGBUFFER;
 int16_t DECAY_DIFFUSION_2_BUFFER[DECAY_DIFFUSION_2_BUFFER_LENGTH];
-
-int16_t DAMPING_FILTER_BUFFER = 0;
-
-dsp_ring_buffer_t DAMPING_DELAY_1_RINGBUFFER;
+dsp_ring_buffer_t DECAY_DIFFUSION_2_RINGBUFFER;
 int16_t DAMPING_DELAY_1_BUFFER[DAMPING_DELAY_1_BUFFER_LENGTH];
-dsp_ring_buffer_t DAMPING_DELAY_2_RINGBUFFER;
+dsp_ring_buffer_t DAMPING_DELAY_1_RINGBUFFER;
 int16_t DAMPING_DELAY_2_BUFFER[DAMPING_DELAY_2_BUFFER_LENGTH];
+dsp_ring_buffer_t DAMPING_DELAY_2_RINGBUFFER;
 
-int16_t REVERBERANCE_BUFFER = 0;
-
-float decay_values[3] = {0.3f, 0.6f, 0.9f};
+float decay_values[3] = {0.25f, 0.5f, 0.5f};
 
 inline int16_t delay_ringbuffer(int16_t x, dsp_ring_buffer_t *buffer)
 {
@@ -182,7 +181,7 @@ int16_t reverberance_filter_process(int16_t x, uint8_t decay_mode)
 	if (decay_mode == 0 || decay_mode > 3)
 		return x;
 
-	reverb_init = bandwith_filter(x * 0.5f, &BANDWIDTH_FILTER_BUFFER, BANDWIDTH_VAL);
+	reverb_init = bandwith_filter(x * 0.5f, &BANDWIDTH_FILTER_BUFFER, BANDWIDTH_GAIN);
 	reverb_init = allpass_iir_filter_ringbuffer(reverb_init, &INPUT_DIFFUSION_1_RINGBUFFER, INPUT_DIFFUSION_1_2_GAIN);
 	reverb_init = allpass_iir_filter_ringbuffer(reverb_init, &INPUT_DIFFUSION_2_RINGBUFFER, INPUT_DIFFUSION_1_2_GAIN);
 	reverb_init = allpass_iir_filter_ringbuffer(reverb_init, &INPUT_DIFFUSION_3_RINGBUFFER, INPUT_DIFFUSION_3_4_GAIN);
@@ -200,6 +199,16 @@ int16_t reverberance_filter_process(int16_t x, uint8_t decay_mode)
 
 	REVERBERANCE_BUFFER = reverb_feedback;
 
+	// cap reverb out to prevent overflow
+	reverb_out = -((INT16_MAX / 2) + 1) >= reverb_out ? -((INT16_MAX / 2) + 1) : reverb_out;
+	reverb_out = ((INT16_MAX / 2) + 1) <= reverb_out ? ((INT16_MAX / 2) + 1) : reverb_out;
+
 	// dry / wet ratio
-	return (x * 2.0f * 0.5f) + (reverb_out * 0.5f);
+	if (decay_mode == 1) {
+		return (x * 0.75f) + (reverb_out * 0.25f);
+	} else if (decay_mode == 2) {
+		return (x * 0.5f) + (reverb_out * 0.5f);
+	}
+
+	return reverb_out;
 }
